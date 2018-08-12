dc6c9669003022420564b7c5df5cb1da7cd31851
OlegWorld <osmironov@gmail.com>
2018-08-12 12:48:15 +0300

    document ready

--- document.h
+++ document.h
@@ -14,52 +14,19 @@ public:
 
     document() = default;
 
-    document(const std::string& filename) {
-        std::ifstream ifs(filename);
-
-        for(std::string line; std::getline(ifs, line);) {
-            read_figure(line);
-        }
-    }
+    document(const std::string& filename);
 
     ~document() = default;
 
-    void add_figure(figure* f) {
-        m_figures.emplace_back(f);
-    }
+    void add_figure(figure* f);
 
-    void delete_figure(const figure* f) {
-        auto it = std::find_if(m_figures.begin(), m_figures.end(), [&f](const auto& fig){ return fig.get() == f; });
-        m_figures.erase(it);
-    }
+    void delete_figure(const figure* f);
 
 private:
-    void read_figure(std::string& str) {
-        std::istringstream iss(str);
-        std::string name;
-        iss >> name;
-
-        if (name == "line") {
-            m_figures.emplace_back(new line(iss));
-        }
-
-        if (name == "rect") {
-            m_figures.emplace_back(new rect(iss));
-        }
-
-        if (name == "circle") {
-            m_figures.emplace_back(new circle(iss));
-        }
-    }
+    void read_figure(std::string& str);
 
 private:
     std::list<std::unique_ptr<figure>> m_figures;
 };
 
-std::ofstream& operator<<(std::ofstream& ofs, const document& doc) {
-    for (const auto& fig : doc.m_figures) {
-        ofs << *fig;
-    }
-
-    return ofs;
-}
+std::ofstream& operator<<(std::ofstream& ofs, const document& doc);
